6fba266ab1acb1a6d1b42bf872f36a3a254c4996
Kirill Filonchuk <kir.realtor@gmail.com>
2021-09-03 17:47:30 +0300

    ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -57,6 +57,24 @@ class App extends Component {
     return visibleContact;
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsetContacts = JSON.parse(contacts);
+
+    if (parsetContacts) {
+      this.setState({
+        contacts: [...parsetContacts],
+      });
+    }
+  }
+  componentDidUpdate(prevState) {
+    // console.log('prevState.contacts', prevState.contacts);
+    // console.log('this.state.contacts', this.state.contacts);
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     // const normalizedFilter = this.state.filter.toLowerCase();
     // const visibleContact = this.state.contacts.filter(con =>
